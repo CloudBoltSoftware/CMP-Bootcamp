d84256d7ab043449e7aa97e99d2454522fb5d38e
Rick Kilcoyne <rkilcoyne@cloudbolt.io>
2023-02-01 08:55:47 -0500

    Added logging and output to template

--- src/src/actions/cbplugin_template/cbplugin_template.py
+++ src/src/actions/cbplugin_template/cbplugin_template.py
@@ -7,13 +7,21 @@ if __name__ == '__main__':
     django.setup()
 
 
+from infrastructure.models import Server
+from common.methods import set_progress
+
+from utilities.logger import ThreadLogger
+logger = ThreadLogger(__name__)
+
 
 def run():
-    from infrastructure.models import Server
     servers = Server.objects.filter(status="ACTIVE")
     for server in servers:
-        print(server.id, server.hostname)
+        msg = f"{server.id}, {server.hostname}"
+        set_progress(msg)  # output to job progress
+        logger.info(msg)   # output to job log
 
+    return "SUCCESS", msg, ""  # output message to job results
 
 if __name__ == '__main__':
     run()
