d04524d2c60267a47e019175be35e1de1edd6a7f
R.A. Kilcoyne <rkilcoyne@cloudbolt.io>
2023-09-06 14:06:00 -0400

    Update install_packages.py
    fixed typo

--- src/cb-repo/server-actions/install_packages.py
+++ src/cb-repo/server-actions/install_packages.py
@@ -6,7 +6,7 @@ import subprocess
 # Convert multi-value field to space-separated list in cmd
 packages = eval(PACKAGES_TO_INSTALL)
 packages = " ".join(packages)
-cmd = "yum install -y {packages}".format()
+cmd = "yum install -y {}".format(packages)
 
 # Print the command we're running to the log
 print("RUNNING: {}".format(cmd))
